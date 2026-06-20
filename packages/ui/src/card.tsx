f5b2e4e3533366e9a42b2fc432f5d8f940f06333
Anjney Lawaniya <anjneylawaniya@gmail.com>
2026-06-20 14:48:37 +0530

    cleanup: package/ui/src folder updated

--- packages/ui/src/card.tsx
+++ packages/ui/src/card.tsx
@@ -1,27 +1,13 @@
-import { type JSX } from "react";
+import * as React from "react";
 
-export function Card({
-  className,
-  title,
-  children,
-  href,
-}: {
-  className?: string;
-  title: string;
+export interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
   children: React.ReactNode;
-  href: string;
-}): JSX.Element {
+}
+
+export function Card({ children, className, ...rest }: CardProps) {
   return (
-    <a
-      className={className}
-      href={`${href}?utm_source=create-turbo&utm_medium=basic&utm_campaign=create-turbo"`}
-      rel="noopener noreferrer"
-      target="_blank"
-    >
-      <h2>
-        {title} <span>-&gt;</span>
-      </h2>
-      <p>{children}</p>
-    </a>
+    <div className={className} {...rest}>
+      {children}
+    </div>
   );
 }
